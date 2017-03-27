cbb07abae5b657a84abb360caaaaa5f71ff81604
Frederico Sanches <fred.sanches@gmail.com>
2017-03-27 17:31:39 +0100

    Scaffold Task Model

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,8 @@
+module UsersHelper
+  def gravatar_for(user, attr_hash)
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+      image_tag(gravatar_url, class: attr_hash[:class], id: attr_hash[:id],
+          "data-toggle" => attr_hash["data-toggle"])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :tasks
+  get 'pages/home'
+
+  root to: 'pages#home'
+
+  get 'pages/contact'
+
+  get 'pages/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
